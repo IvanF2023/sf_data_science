8d46c46b8dcf656e900dbfb584c950a2cb9ad9ca
Ivan_F <ivanfedoseyev@inbox.ru>
2023-01-05 17:51:03 +0300

    edited

--- Project_0/game_v2.py
+++ Project_0/game_v2.py
@@ -1,24 +1,24 @@
 import numpy as np
-def predict_number(number:int=1) -> int:
-    count = 0
-    number = np.random.randint(1, 101)
-    print("Загадано число от 1 до 100")
-    min = 0
-    max = 100
-    while True:
-        predict_numer = round((min+max)/2)
-     
-        count += 1
-        if number == predict_numer:
-            break   
-        elif number > predict_numer:
-            min = predict_numer
-            print(f"Угадываемое число больше {predict_numer}")
-            round((max + min) / 2)
-        elif number < predict_numer:
-            max = predict_numer
-        print(f"Угадываемое число меньше {predict_numer}")
-        round((max+min)/2)
 
-    print(f"Вы угадали число {number} за {count} попыток.")
-predict_number()
+count = 0
+number = np.random.randint(1, 101)
+print("Загадано число от 1 до 100")
+min = 0
+max = 100
+while True:
+    predict = round((min+max)/2)
+    #predict = int(input())
+    count += 1
+    if number == predict:
+        break
+    elif number > predict:
+        min = predict
+        print(f"Угадываемое число больше {predict}")
+        print(f'Алгоритм бинарного поиска рекомендует вам число:{round((max + min) / 2)}')
+    elif number < predict:
+        max = predict
+        print(f"Угадываемое число меньше {predict}")
+        print(f'Алгоритм бинарного поиска рекомендует вам число:{round((max+min)/2)}')
+
+
+print(f"Вы угадали число {number} за {count} попыток.")
