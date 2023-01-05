495f8b72e10e668caef9f94483c925856566fff4
Ivan_F <ivanfedoseyev@inbox.ru>
2023-01-05 14:25:58 +0300

    edited

--- Project_0/game_v2.py
+++ Project_0/game_v2.py
@@ -1,47 +1,24 @@
 import numpy as np
 
-def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+count = 0
+number = np.random.randint(1, 101)
+print("Загадано число от 1 до 100")
+min = 0
+max = 100
+while True:
+    predict_numer = round((min+max)/2)
+    #predict = int(input())
+    count += 1
+    if number == predict_numer:
+        break
+    elif number > predict_numer:
+        min = predict_numer
+        print(f"Угадываемое число больше {predict_numer}")
+        print(f'Алгоритм поиска рекомендует вам число:{round((max + min) / 2)}')
+    elif number < predict_numer:
+        max = predict_numer
+        print(f"Угадываемое число меньше {predict_numer}")
+        print(f'Алгоритм поиска рекомендует вам число:{round((max+min)/2)}')
 
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
 
-    Returns:
-        int: Число попыток
-    """
-
-    count = 0
-
-    while True:
-        count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
-        if number == predict_number:
-            break # выход из цикла, если угадали
-    return(count)
-
-print(f'Количество попыток: {random_predict()}')
-
-def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
-
-    Args:
-        random_predict ([type]): функция угадывания
-
-    Returns:
-        int: среднее количество попыток
-    """
-
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
-
-    for number in random_array:
-        count_ls.append(random_predict(number))
-
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
-
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
-
-# RUN
-score_game(random_predict)
+print(f"Вы угадали число {number} за {count} попыток.")
